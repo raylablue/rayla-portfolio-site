9365a9d9e817f9ecf34bc947bc3eda2d4b3a0e94
Ray <rachel!7.22@gmail.com>
2022-03-09 19:50:03 -0800

    created placeholder content blocking and layout grid with mui grid.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Button } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import themeDark from './styles/themeDark';
 import themeLight from './styles/themeLight';
-import ProfilePic from './atomicDesign/atoms/ProfilePic';
+import ProfilePic from './atomicDesign/atoms/ProfilePic/ProfilePic';
 
 const App = () => {
   const [light, setLight] = useState(false);
@@ -20,7 +20,25 @@ const App = () => {
       >
         Toggle Theme
       </Button>
-      <ProfilePic />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8} md={4}>
+            <ProfilePic />
+            <div>this will be icons to social accounts</div>
+            <div>this will be a link to my resume</div>
+          </Grid>
+
+          <Grid item md={6}>
+            <div>
+              this will be a list of my portfolio items
+              <div>Portfolio Item 1</div>
+              <div>Portfolio Item 2</div>
+              <div>Portfolio Item 3</div>
+            </div>
+          </Grid>
+
+        </Grid>
+      </Box>
     </ThemeProvider>
   );
 };
--- src/atomicDesign/atoms/ProfilePic/ProfilePic.tsx
+++ src/atomicDesign/atoms/ProfilePic/ProfilePic.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import './ProfilePic.css';
 
 const ProfilePic = () => (
   <Box>
     {/* eslint-disable-next-line global-require */}
-    <img alt="Ray Blue" src={require('../../assets/Ray.jpeg')} />
+    <img alt="Ray Blue" src={require('../../../assets/Ray.jpeg')} className="photo" />
   </Box>
 );
 
