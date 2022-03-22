a30718bc8ba3eb886e8fe270fe8b8ac2a9f55dbd
Ray <rachel!7.22@gmail.com>
2022-03-21 21:47:09 -0700

    socical icons can set size

---
+++ src/atomicDesign/atoms/Icon.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+import CodeIcon from '@mui/icons-material/Code';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const nameToIcon = {
+  codeIcon: <CodeIcon />,
+  githubIcon: <GitHubIcon />,
+  linkedinIcon: <LinkedInIcon />,
+  twitterIcon: <TwitterIcon />,
+};
+
+interface IIconsProps {
+  name: 'codeIcon' | 'githubIcon' | 'linkedinIcon' | 'twitterIcon';
+  size?: 'inherit' | 'large' | 'medium' | 'small',
+}
+
+const DEFAULT_SIZE = 'inherit';
+
+const Icon = ({ name, size }: IIconsProps) => useMemo(() => ({
+  ...nameToIcon[name],
+  props: {
+    fontSize: size || DEFAULT_SIZE,
+  },
+}), [name, size]);
+
+export default Icon;
--- src/atomicDesign/molecules/SocialIcons.tsx
+++ src/atomicDesign/molecules/SocialIcons.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from '@mui/material';
-import Icons from '../atoms/Icons';
+import Icon from '../atoms/Icon';
 
 const SocialIcons = () => (
   <div>
-    <Link target="_blank" href="https://github.com/raylablue">
-      <Icons name="githubIcon" />
+    <Link
+      color="secondary"
+      target="_blank"
+      href="https://github.com/raylablue"
+    >
+      <Icon size="large" name="githubIcon" />
+      test
     </Link>
     <br />
     <Link
       target="_blank"
       href="https://www.linkedin.com/in/rayla-ray-blue-99b70223/"
     >
-      LinkedIn Link
+      <Icon size="large" name="linkedinIcon" />
     </Link>
     <br />
     <Link target="_blank" href="https://twitter.com/RaylaDev">
-      Twitter Link
+      <Icon size="large" name="twitterIcon" />
     </Link>
   </div>
 );
